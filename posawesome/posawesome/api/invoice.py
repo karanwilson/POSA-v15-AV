07c55972d024f9e8b176e9637af9a6796e383f34
Karan Wilson <karan.wilson@auroville.org.in>
2024-09-04 22:25:35 +0530

    fix: condition in validation for Offline FS Pay invoices

--- posawesome/posawesome/api/invoice.py
+++ posawesome/posawesome/api/invoice.py
@@ -249,7 +249,8 @@ def validate_shift(doc):
     if doc.posa_pos_opening_shift and doc.pos_profile and doc.is_pos:
         # check if shift is open
         shift = frappe.get_cached_doc("POS Opening Shift", doc.posa_pos_opening_shift)
-        if shift.status != "Open":
+        # factoring in Offline FS payments which are processed after the POS shift is closed
+        if shift.status != "Open" and not doc.custom_fs_transfer_status:
             frappe.throw(_("POS Shift {0} is not open").format(shift.name))
         # check if shift is for the same profile
         if shift.pos_profile != doc.pos_profile:
